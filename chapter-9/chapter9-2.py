18d60e9e2e6cc3760d92c3aed30dd3670b685932
Aslan-Zhou <2894625553@qq.com>
2022-02-06 15:53:20 +0800

    nothing

--- chapter-9/chapter9-2.py
+++ chapter-9/chapter9-2.py
@@ -6,7 +6,7 @@ root.title("ch9-2")
 Scale(root, from_=0, to=10,
       troughcolor="green",
       width=30, length=300,
-      tickinterval=2, bg="green",
+      tickinterval=2,
       orient=HORIZONTAL,
       label="My Scale").pack()
 
